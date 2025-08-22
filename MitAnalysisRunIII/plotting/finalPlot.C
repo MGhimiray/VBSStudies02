a088e432d5ac917f730ab4d4a4ebfa4fe21f0893
Monika Ghimiray <ghimiraymonika@gmail.com>
2025-08-22 13:33:09 +0200

    yields

--- MitAnalysisRunIII/plotting/finalPlot.C
+++ MitAnalysisRunIII/plotting/finalPlot.C
@@ -22,7 +22,7 @@ bool isLogSpecial = false;
 bool isLogX = false;
 bool printRatios = false;
 bool printYieldsBinByBin = false;
-TString OUTPUT_DIR = "/mnt/home/mghimiray/VBSStudies/Outputs_VBS/plots2/";
+TString OUTPUT_DIR = "/mnt/home/mghimiray/VBSStudies/Outputs_VBS/OriginalTT/plots/";
 
 void eraselabel(TPad *p,Double_t h){
   p->cd();
