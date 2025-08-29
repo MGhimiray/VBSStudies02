f734ff5dedb87890e68fbbebbc4011ea24392a5b
Monika Ghimiray <ghimiraymonika@gmail.com>
2025-08-29 11:23:19 +0200

    nothing to commit

--- MitAnalysisRunIII/rdf/macros/sswwAnalysis.py
+++ MitAnalysisRunIII/rdf/macros/sswwAnalysis.py
@@ -9,7 +9,7 @@ from utilsSelection import selectionTauVeto, selectionPhoton, selectionJetMet, s
 import tmva_helper_xml
 
 correctionString = "_correction"
-makeDataCards = 0 # 1 (mjj diff), 2 (mll diff), 3 (njets diff), 4 (detajj diff), 5 (dphijj diff), 6 (mjj), 7 (mll), 8 (detajj), 9 (dphijj)
+makeDataCards = 1 # 1 (mjj diff), 2 (mll diff), 3 (njets diff), 4 (detajj diff), 5 (dphijj diff), 6 (mjj), 7 (mll), 8 (detajj), 9 (dphijj)
 genVBSSel = makeDataCards
 if(genVBSSel == 6):
     genVBSSel = 1
@@ -937,7 +937,7 @@ def analysis(df,count,category,weight,year,PDType,isData,whichJob,nTheoryReplica
                     histoMVA[j][x].SetBinContent(i+1,        histoMVA[j][x].GetBinContent(i+1)+       histo2D[j][x].GetBinContent(i+1,1))
                     histoMVA[j][x].SetBinError  (i+1,pow(pow(histoMVA[j][x].GetBinError  (i+1),2)+pow(histo2D[j][x].GetBinError  (i+1,1),2),0.5))
 
-    output_dir2 = "/mnt/home/mghimiray/VBSStudies/Outputs_VBS/DataDriven/fillhisto_sswwAnalysis1/" # Defining output directory for histograms
+    output_dir2 = "/mnt/home/mghimiray/VBSStudies/Outputs_VBS/DataDriven/fillhisto_sswwAnalysis1001/" # Defining output directory for histograms
     os.makedirs(output_dir2, exist_ok=True)
     myfile = ROOT.TFile("{3}/fillhisto_sswwAnalysis1001_sample{0}_year{1}_job{2}.root".format(count,year,whichJob,output_dir2),'RECREATE')
     for i in range(nCat):
