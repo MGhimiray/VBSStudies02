14beb8f224e1ef18a206e316bfefb79c2306b694
MGhimiray <monikaghimiray@gmail.com>
2025-09-10 22:16:34 +0200

    works for full run3

--- MitAnalysisRunIII/rdf/macrosdata/analysis_singularity_slurm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-source /cvmfs/cms.cern.ch/cmsset_default.sh;export APPTAINER_BIND="${thisDir},/ceph/submit";cmssw-el9 --command-to-run ./analysis_slurm_with_singularity.sh $1 $2 $3 $4 $5
