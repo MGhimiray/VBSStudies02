6312d1f1c19adfc3c1c5b65a3dcac35cf9957866
MGhimiray <monikaghimiray@gmail.com>
2025-09-15 19:26:38 +0200

    for datacards

--- MitAnalysisRunIII/rdf/macrosTT/makeSSWWDataCards.C
+++ MitAnalysisRunIII/rdf/macrosTT/makeSSWWDataCards.C
@@ -17,7 +17,7 @@
 // whichAna = 0 (SSWW), fidAna = 0/2/4 (WW), 1/3/5 (WWb)
 // whichAna = 0 (WZ), fidAna = 0 (WZ), 1 (WZb)
 
-void makeSSWWDataCards(int whichAna = 0, int fidAna = 0, TString InputDir = "anaZ", TString anaSel = "sswwAnalysis1001", int year = 20220){
+void makeSSWWDataCards(int whichAna = 0, int fidAna = 1, TString InputDir = "/mnt/home/mghimiray/VBSStudies/Outputs_VBS/OriginalTT/1001/merge1/", TString anaSel = "sswwAnalysis1001", int year = 20240){
 
   if(fidAna < 0 || fidAna > 6) printf("Wrong fidAna(%d)\n",fidAna);
 
@@ -32,6 +32,7 @@ void makeSSWWDataCards(int whichAna = 0, int fidAna = 0, TString InputDir = "ana
   else if(year == 20221) {triggerEffUnc = 1.005; theYear = 2022;}
   else if(year == 20230) {triggerEffUnc = 1.005; theYear = 2023;}
   else if(year == 20231) {triggerEffUnc = 1.005; theYear = 2023;}
+  else if(year == 20240) {triggerEffUnc = 1.005; theYear = 2024;}
   else {printf("Wrong year!\n"); return;}
 
   int jumpValue = 200;
@@ -460,7 +461,10 @@ void makeSSWWDataCards(int whichAna = 0, int fidAna = 0, TString InputDir = "ana
     additionalSuffix = "_alt";
   }
 
-  TString outputLimits = Form("output_%s_%d_bin%d%s.root",anaSel.Data(),year,fidAna,additionalSuffix.Data());
+
+  TString outdir = "/mnt/home/mghimiray/VBSStudies/Outputs_VBS/OriginalTT/1001/datacards";
+  std::filesystem::create_directories(outdir.Data());
+  TString outputLimits = Form("%s/output_%s_%d_bin%d%s.root", outdir.Data(), anaSel.Data(), year, fidAna, additionalSuffix.Data());
   outputFile = new TFile(outputLimits, "RECREATE");
   outputFile->cd();
   for(unsigned ic=kPlotData; ic!=nPlotCategories; ic++) {
@@ -483,7 +487,7 @@ void makeSSWWDataCards(int whichAna = 0, int fidAna = 0, TString InputDir = "ana
 
   // Filling datacards txt file
   char outputLimitsCard[200];
-  sprintf(outputLimitsCard,"datacard_%s_%d_bin%d%s.txt",anaSel.Data(),year,fidAna,additionalSuffix.Data());
+  sprintf(outputLimitsCard,"%s/datacard_%s_%d_bin%d%s.txt", outdir.Data(),anaSel.Data(),year,fidAna,additionalSuffix.Data());
   ofstream newcardShape;
   newcardShape.open(outputLimitsCard);
   newcardShape << Form("imax * number of channels\n");
